4fc974546e1a5f66f4213527608b7edf68764913
Chamsol Kim <kcsol1005@gmail.com>
2024-05-27 09:41:00 +0900

    #3.0 project setup

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+      ),
+      home: Container(),
+    );
+  }
+}
