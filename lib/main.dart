b1a5cf4aa38e5b4dbc19761c5bea6acfd86eba85
Chamsol Kim <kcsol1005@gmail.com>
2024-05-27 09:50:40 +0900

    #3.1 Constants - Sizes, Gaps
    - Size, padding, margin 등을 미리 정의해 두고 사용
    - 앱 전체에 일관된 UI와 layout을 만들 수 있음

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -15,7 +17,16 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: const Padding(
+        padding: EdgeInsets.all(Sizes.size14),
+        child: Row(
+          children: [
+            Text('Hello'),
+            Gaps.h10,
+            Text('hello'),
+          ],
+        ),
+      ),
     );
   }
 }
