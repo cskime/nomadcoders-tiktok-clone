b8a8f7a26c0b5a85e0a27d1a05e41450c1d2b0f6
Chamsol Kim <kcsol1005@gmail.com>
2024-05-28 21:28:58 +0900

    #7.8 VideoPlayerController - setLooping, CircleAvatar,

---
+++ lib/features/videos/widgets/video_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+class VideoButton extends StatelessWidget {
+  const VideoButton({
+    super.key,
+    required this.icon,
+    required this.title,
+  });
+
+  final IconData icon;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FaIcon(
+          icon,
+          color: Colors.white,
+          size: Sizes.size24,
+        ),
+        Gaps.v4,
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_button.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -51,6 +53,7 @@ class _VideoPostState extends State<VideoPost>
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
     _videoPlayerController.addListener(_onVideoChange);
   }
 
@@ -123,7 +126,64 @@ class _VideoPostState extends State<VideoPost>
                 ),
               ),
             ),
-          )
+          ),
+          const Positioned(
+            left: 16,
+            bottom: 20,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '@joey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: Sizes.size20,
+                  ),
+                ),
+                Gaps.v14,
+                Text(
+                  'This is my house in Thailand!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: Sizes.size16,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Positioned(
+            right: 16,
+            bottom: 20,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/42177438?v=4',
+                  ),
+                  child: Text('JY'),
+                ),
+                Gaps.v20,
+                VideoButton(
+                  icon: FontAwesomeIcons.solidHeart,
+                  title: '2.9M',
+                ),
+                Gaps.v20,
+                VideoButton(
+                  icon: FontAwesomeIcons.solidComment,
+                  title: '3.3K',
+                ),
+                Gaps.v20,
+                VideoButton(
+                  icon: FontAwesomeIcons.share,
+                  title: 'Share',
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
