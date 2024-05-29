52a76cb364e1bfff710de7fd2d386126ede99696
Chamsol Kim <kcsol1005@gmail.com>
2024-05-29 16:50:18 +0900

    #10.3 Dismissable-onDismissed, DismissDirection

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -3,8 +3,21 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
-class ActivityScreen extends StatelessWidget {
+class ActivityScreen extends StatefulWidget {
   const ActivityScreen({super.key});
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  final _notifications = List.generate(20, (index) => '${index}h');
+
+  void _onDismissed(String notification) {
+    setState(() {
+      _notifications.remove(notification);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,79 +39,82 @@ class ActivityScreen extends StatelessWidget {
             ),
           ),
           Gaps.v10,
-          Dismissible(
-            key: const ValueKey('value'),
-            background: Container(
-              color: Colors.green,
-              alignment: Alignment.centerLeft,
-              child: const Padding(
-                padding: EdgeInsets.only(left: Sizes.size10),
-                child: FaIcon(
-                  FontAwesomeIcons.checkDouble,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            secondaryBackground: Container(
-              color: Colors.red,
-              alignment: Alignment.centerRight,
-              child: const Padding(
-                padding: EdgeInsets.only(left: Sizes.size10),
-                child: FaIcon(
-                  FontAwesomeIcons.trashCan,
-                  color: Colors.white,
-                  size: Sizes.size32,
-                ),
-              ),
-            ),
-            child: ListTile(
-              leading: Container(
-                width: Sizes.size52,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                  border: Border.all(
-                    color: Colors.grey.shade400,
-                    width: Sizes.size2,
-                  ),
-                ),
-                child: const Center(
+          for (final notification in _notifications)
+            Dismissible(
+              key: ValueKey(notification),
+              onDismissed: (direction) => _onDismissed(notification),
+              background: Container(
+                color: Colors.green,
+                alignment: Alignment.centerLeft,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: Sizes.size10),
                   child: FaIcon(
-                    FontAwesomeIcons.bell,
-                    color: Colors.black,
+                    FontAwesomeIcons.checkDouble,
+                    color: Colors.white,
+                    size: Sizes.size32,
                   ),
                 ),
               ),
-              title: RichText(
-                text: TextSpan(
-                  text: 'Account updates:',
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                    fontSize: Sizes.size16,
+              secondaryBackground: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                child: const Padding(
+                  padding: EdgeInsets.only(left: Sizes.size10),
+                  child: FaIcon(
+                    FontAwesomeIcons.trashCan,
+                    color: Colors.white,
+                    size: Sizes.size32,
                   ),
-                  children: [
-                    const TextSpan(
-                      text: ' Upload longer videos',
-                      style: TextStyle(fontWeight: FontWeight.normal),
+                ),
+              ),
+              child: ListTile(
+                minVerticalPadding: Sizes.size16,
+                leading: Container(
+                  width: Sizes.size52,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.grey.shade400,
+                      width: Sizes.size2,
                     ),
-                    TextSpan(
-                      text: ' 1h',
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        color: Colors.grey.shade500,
+                  ),
+                  child: const Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.bell,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                title: RichText(
+                  text: TextSpan(
+                    text: 'Account updates:',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size16,
+                    ),
+                    children: [
+                      const TextSpan(
+                        text: ' Upload longer videos',
+                        style: TextStyle(fontWeight: FontWeight.normal),
                       ),
-                    ),
-                  ],
+                      TextSpan(
+                        text: ' $notification',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                trailing: const FaIcon(
+                  FontAwesomeIcons.chevronRight,
+                  size: Sizes.size14,
                 ),
               ),
-              trailing: const FaIcon(
-                FontAwesomeIcons.chevronRight,
-                size: Sizes.size14,
-              ),
-            ),
-          )
+            )
         ],
       ),
     );
