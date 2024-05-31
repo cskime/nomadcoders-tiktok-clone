6e3d70c2f63e192b097b89f18cc164477651e5ae
Chamsol Kim <kcsol1005@gmail.com>
2024-05-31 15:53:11 +0900

    #12.4 SliverPersistentHeader: sticky header 만들기, SliverToBoxAdapter: 일반 widget을 sliver로 사용하기

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -14,7 +14,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          pinned: true,
+          // pinned: true,
           stretch: true,
           backgroundColor: Colors.teal,
           collapsedHeight: 80,
@@ -31,9 +31,19 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ],
           ),
         ),
+        const SliverToBoxAdapter(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 20,
+              ),
+            ],
+          ),
+        ),
         SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
-            childCount: 20,
+            childCount: 50,
             (context, index) => Container(
               color: Colors.amber[100 * (index % 9)],
               child: Center(child: Text('Item $index')),
@@ -41,9 +51,14 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           ),
           itemExtent: 100,
         ),
+        SliverPersistentHeader(
+          delegate: CustomDelegate(),
+          // pinned: true,
+          floating: true,
+        ),
         SliverGrid(
           delegate: SliverChildBuilderDelegate(
-            childCount: 20,
+            childCount: 50,
             (context, index) => Container(
               color: Colors.amber[100 * (index % 9)],
               child: Center(child: Text('Item $index')),
@@ -58,5 +73,35 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         ),
       ],
     );
+  }
+}
+
+class CustomDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.indigo,
+      child: const FractionallySizedBox(
+        heightFactor: 1,
+        child: Center(
+          child: Text(
+            'Header Title!!',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 150;
+
+  @override
+  double get minExtent => 80;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
