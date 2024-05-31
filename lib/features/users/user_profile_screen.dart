2e60dbe9baf1a57cc59eb4e84633c4207a0b879d
Chamsol Kim <kcsol1005@gmail.com>
2024-05-31 17:01:36 +0900

    #12.6 DefaultTabController, TabBar, TabBarView 사용, 중첩 scroll을 구현할 수 없는 문제 (CustomScrollView + GridView)

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -13,80 +13,170 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: const Text('Joey'),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const FaIcon(
-                FontAwesomeIcons.gear,
-                size: Sizes.size20,
-              ),
-            )
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Column(
-            children: [
-              const CircleAvatar(
-                radius: Sizes.size52,
-                foregroundColor: Colors.blue,
-                foregroundImage: NetworkImage(
-                  'https://avatars.githubusercontent.com/u/42177438?v=4',
+    return DefaultTabController(
+      length: 2,
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text('Joey'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const FaIcon(
+                  FontAwesomeIcons.gear,
+                  size: Sizes.size20,
                 ),
-                child: Text('JY'),
-              ),
-              Gaps.v20,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    '@JY',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: Sizes.size18,
-                    ),
-                  ),
-                  Gaps.h5,
-                  FaIcon(
-                    FontAwesomeIcons.solidCircleCheck,
-                    color: Colors.blue.shade300,
-                    size: Sizes.size16,
-                  )
-                ],
-              ),
-              Gaps.v24,
-              const SizedBox(
-                height: Sizes.size48,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _VerticalLabel(title: '97', subtitle: 'Following'),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    _VerticalLabel(title: '10M', subtitle: 'Followers'),
-                    VerticalDivider(
-                      width: Sizes.size32,
-                      thickness: Sizes.size1,
-                      color: Colors.grey,
-                      indent: Sizes.size14,
-                      endIndent: Sizes.size14,
-                    ),
-                    _VerticalLabel(title: '194.3M', subtitle: 'Likes'),
-                  ],
-                ),
-              ),
+              )
             ],
           ),
-        )
-      ],
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CircleAvatar(
+                  radius: Sizes.size52,
+                  foregroundColor: Colors.blue,
+                  foregroundImage: NetworkImage(
+                    'https://avatars.githubusercontent.com/u/42177438?v=4',
+                  ),
+                  child: Text('JY'),
+                ),
+                Gaps.v20,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      '@JY',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: Sizes.size18,
+                      ),
+                    ),
+                    Gaps.h5,
+                    FaIcon(
+                      FontAwesomeIcons.solidCircleCheck,
+                      color: Colors.blue.shade300,
+                      size: Sizes.size16,
+                    )
+                  ],
+                ),
+                Gaps.v24,
+                const SizedBox(
+                  height: Sizes.size48,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _VerticalLabel(title: '97', subtitle: 'Following'),
+                      VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        color: Colors.grey,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      _VerticalLabel(title: '10M', subtitle: 'Followers'),
+                      VerticalDivider(
+                        width: Sizes.size32,
+                        thickness: Sizes.size1,
+                        color: Colors.grey,
+                        indent: Sizes.size14,
+                        endIndent: Sizes.size14,
+                      ),
+                      _VerticalLabel(title: '194.3M', subtitle: 'Likes'),
+                    ],
+                  ),
+                ),
+                Gaps.v14,
+                FractionallySizedBox(
+                  widthFactor: 0.33,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: Sizes.size12,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(Sizes.size4),
+                    ),
+                    child: const Text(
+                      'Follow',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Gaps.v14,
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: Sizes.size32),
+                  child: Text(
+                    'All highlights and where to watch live matches on FIFA+',
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Gaps.v14,
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.link,
+                      size: Sizes.size12,
+                    ),
+                    Gaps.h4,
+                    Text(
+                      'https://noamdcoders.co',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Gaps.v20,
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(
+                        color: Colors.grey.shade200,
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: const TabBar(
+                    labelColor: Colors.black,
+                    labelPadding: EdgeInsets.symmetric(vertical: 8),
+                    indicatorColor: Colors.black,
+                    indicatorSize: TabBarIndicatorSize.label,
+                    tabs: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: Icon(Icons.grid_4x4_rounded),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
+                        child: FaIcon(FontAwesomeIcons.heart),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width,
+                  height: MediaQuery.sizeOf(context).height,
+                  child: const TabBarView(
+                    children: [
+                      Center(
+                        child: Text('Page1'),
+                      ),
+                      Center(
+                        child: Text('Page2'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
