e804dbef7f1bcbbe00e72ac56bfba8556566914f
Chamsol Kim <kcsol1005@gmail.com>
2024-06-06 15:17:25 +0900

    #14.6 Code challenge - settings screen에 max width constraint 적용

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/breakpoints.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -26,220 +27,225 @@ class _SettingsScreenState extends State<SettingsScreen> {
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: ListView(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+      body: Center(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxWidth: Breakpoints.small),
+          child: ListView(
             children: [
-              Column(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  const Text('Material'),
-                  Checkbox(
-                    value: _notifications,
-                    onChanged: _notificationChanged,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const Text('Cupertino'),
-                  CupertinoCheckbox(
-                    value: _notifications,
-                    onChanged: _notificationChanged,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const Text('Adaptive'),
-                  Checkbox.adaptive(
-                    value: _notifications,
-                    onChanged: _notificationChanged,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          CheckboxListTile(
-            value: _notifications,
-            title: const Text('ListTile with material design checkbox'),
-            checkColor: Colors.white,
-            activeColor: Colors.black,
-            onChanged: _notificationChanged,
-          ),
-          CheckboxListTile.adaptive(
-            value: _notifications,
-            title: const Text('ListTile with platform preferred checkbox'),
-            onChanged: _notificationChanged,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  const Text('Material'),
-                  Switch(
-                    value: _notifications,
-                    onChanged: _notificationChanged,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const Text('Cupertino'),
-                  CupertinoSwitch(
-                    value: _notifications,
-                    onChanged: _notificationChanged,
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  const Text('Adaptive'),
-                  Switch.adaptive(
-                    value: _notifications,
-                    onChanged: _notificationChanged,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SwitchListTile(
-            value: _notifications,
-            title: const Text('ListTile with material design switch'),
-            onChanged: _notificationChanged,
-          ),
-          SwitchListTile.adaptive(
-            value: _notifications,
-            title: const Text('ListTile with platform preferred switch'),
-            onChanged: _notificationChanged,
-          ),
-          ListTile(
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: '1.0',
-              applicationLegalese: 'All rights reserved',
-            ),
-            title: const Text(
-              'About',
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: const Text('About this app...'),
-          ),
-          const AboutListTile(),
-          ListTile(
-            onTap: () async {
-              final date = await showDatePicker(
-                context: context,
-                initialDate: DateTime.now(),
-                firstDate: DateTime(1950),
-                lastDate: DateTime(2030),
-              );
-              // ignore: avoid_print
-              print(date);
-
-              if (context.mounted) {
-                final time = await showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.now(),
-                );
-                // ignore: avoid_print
-                print(time);
-              }
-
-              if (context.mounted) {
-                final range = await showDateRangePicker(
-                  context: context,
-                  firstDate: DateTime(1950),
-                  lastDate: DateTime(2030),
-                  builder: (context, child) {
-                    return Theme(
-                      data: ThemeData(
-                        useMaterial3: false,
-                        appBarTheme: const AppBarTheme(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.black,
-                        ),
+                  Column(
+                    children: [
+                      const Text('Material'),
+                      Checkbox(
+                        value: _notifications,
+                        onChanged: _notificationChanged,
                       ),
-                      child: child!,
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text('Cupertino'),
+                      CupertinoCheckbox(
+                        value: _notifications,
+                        onChanged: _notificationChanged,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text('Adaptive'),
+                      Checkbox.adaptive(
+                        value: _notifications,
+                        onChanged: _notificationChanged,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              CheckboxListTile(
+                value: _notifications,
+                title: const Text('ListTile with material design checkbox'),
+                checkColor: Colors.white,
+                activeColor: Colors.black,
+                onChanged: _notificationChanged,
+              ),
+              CheckboxListTile.adaptive(
+                value: _notifications,
+                title: const Text('ListTile with platform preferred checkbox'),
+                onChanged: _notificationChanged,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      const Text('Material'),
+                      Switch(
+                        value: _notifications,
+                        onChanged: _notificationChanged,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text('Cupertino'),
+                      CupertinoSwitch(
+                        value: _notifications,
+                        onChanged: _notificationChanged,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text('Adaptive'),
+                      Switch.adaptive(
+                        value: _notifications,
+                        onChanged: _notificationChanged,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SwitchListTile(
+                value: _notifications,
+                title: const Text('ListTile with material design switch'),
+                onChanged: _notificationChanged,
+              ),
+              SwitchListTile.adaptive(
+                value: _notifications,
+                title: const Text('ListTile with platform preferred switch'),
+                onChanged: _notificationChanged,
+              ),
+              ListTile(
+                onTap: () => showAboutDialog(
+                  context: context,
+                  applicationVersion: '1.0',
+                  applicationLegalese: 'All rights reserved',
+                ),
+                title: const Text(
+                  'About',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                subtitle: const Text('About this app...'),
+              ),
+              const AboutListTile(),
+              ListTile(
+                onTap: () async {
+                  final date = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1950),
+                    lastDate: DateTime(2030),
+                  );
+                  // ignore: avoid_print
+                  print(date);
+
+                  if (context.mounted) {
+                    final time = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
                     );
-                  },
-                );
-                // ignore: avoid_print
-                print(range);
-              }
-            },
-            title: const Text('What is your birthday?'),
-          ),
-          ListTile(
-            title: const Text('Log out (iOS)'),
-            textColor: Colors.red,
-            onTap: () => showCupertinoDialog(
-              context: context,
-              builder: (context) => CupertinoAlertDialog(
-                title: const Text('Are you sure?'),
-                content: const Text('Plase dont go'),
-                actions: [
-                  CupertinoDialogAction(
-                    child: const Text('No'),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                  CupertinoDialogAction(
-                    isDestructiveAction: true,
-                    child: const Text('Yes'),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                ],
+                    // ignore: avoid_print
+                    print(time);
+                  }
+
+                  if (context.mounted) {
+                    final range = await showDateRangePicker(
+                      context: context,
+                      firstDate: DateTime(1950),
+                      lastDate: DateTime(2030),
+                      builder: (context, child) {
+                        return Theme(
+                          data: ThemeData(
+                            useMaterial3: false,
+                            appBarTheme: const AppBarTheme(
+                              foregroundColor: Colors.white,
+                              backgroundColor: Colors.black,
+                            ),
+                          ),
+                          child: child!,
+                        );
+                      },
+                    );
+                    // ignore: avoid_print
+                    print(range);
+                  }
+                },
+                title: const Text('What is your birthday?'),
               ),
-            ),
-          ),
-          ListTile(
-            title: const Text('Log out (Android)'),
-            textColor: Colors.red,
-            onTap: () => showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: const Text('Are you sure?'),
-                content: const Text('Plase dont go'),
-                actions: [
-                  IconButton(
-                    icon: const FaIcon(FontAwesomeIcons.car),
-                    onPressed: () => Navigator.of(context).pop(),
+              ListTile(
+                title: const Text('Log out (iOS)'),
+                textColor: Colors.red,
+                onTap: () => showCupertinoDialog(
+                  context: context,
+                  builder: (context) => CupertinoAlertDialog(
+                    title: const Text('Are you sure?'),
+                    content: const Text('Plase dont go'),
+                    actions: [
+                      CupertinoDialogAction(
+                        child: const Text('No'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                      CupertinoDialogAction(
+                        isDestructiveAction: true,
+                        child: const Text('Yes'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
                   ),
-                  TextButton(
-                    child: const Text('Yes'),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
-          ListTile(
-            title: const Text('Log out (iOS / Bottom)'),
-            textColor: Colors.red,
-            onTap: () => showCupertinoModalPopup(
-              context: context,
-              builder: (context) => CupertinoActionSheet(
-                title: const Text('Are you sure?'),
-                message: const Text('Please dont go'),
-                actions: [
-                  CupertinoActionSheetAction(
-                    isDefaultAction: true,
-                    child: const Text('No'),
-                    onPressed: () => Navigator.of(context).pop(),
+              ListTile(
+                title: const Text('Log out (Android)'),
+                textColor: Colors.red,
+                onTap: () => showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Are you sure?'),
+                    content: const Text('Plase dont go'),
+                    actions: [
+                      IconButton(
+                        icon: const FaIcon(FontAwesomeIcons.car),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                      TextButton(
+                        child: const Text('Yes'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
                   ),
-                  CupertinoActionSheetAction(
-                    isDestructiveAction: true,
-                    child: const Text('Yes'),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                ],
+                ),
               ),
-            ),
+              ListTile(
+                title: const Text('Log out (iOS / Bottom)'),
+                textColor: Colors.red,
+                onTap: () => showCupertinoModalPopup(
+                  context: context,
+                  builder: (context) => CupertinoActionSheet(
+                    title: const Text('Are you sure?'),
+                    message: const Text('Please dont go'),
+                    actions: [
+                      CupertinoActionSheetAction(
+                        isDefaultAction: true,
+                        child: const Text('No'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                      CupertinoActionSheetAction(
+                        isDestructiveAction: true,
+                        child: const Text('Yes'),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/main_navigation/main_navigation_screen.dart';
+import 'package:tiktok_clone/features/users/user_profile_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,22 +16,26 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'TikTok Clone',
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          primaryColor: const Color(0xFFE9435A),
-          appBarTheme: const AppBarTheme(
-            foregroundColor: Colors.black,
-            backgroundColor: Colors.white,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontSize: Sizes.size16 + Sizes.size2,
-              fontWeight: FontWeight.w700,
-            ),
+      title: 'TikTok Clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: const Color(0xFFE9435A),
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w700,
           ),
-          useMaterial3: false,
         ),
-        home: const MainNavigationScreen());
+        useMaterial3: false,
+      ),
+      // home: const MainNavigationScreen());
+      home: const Scaffold(
+        body: UserProfileScreen(),
+      ),
+    );
   }
 }
