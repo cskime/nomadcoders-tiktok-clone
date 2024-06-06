6c2e1a11ce85eba074a95888859013f5d50be2f1
Chamsol Kim <kcsol1005@gmail.com>
2024-06-06 15:04:46 +0900

    #14.5 ConstrainedBox - 특정 widget에 constraint 설정

--- lib/constants/breakpoints.dart
+++ lib/constants/breakpoints.dart
@@ -1,7 +1,7 @@
 abstract final class Breakpoints {
-  static const small = 640;
-  static const medium = 768;
-  static const large = 1024;
-  static const xlarge = 1280;
-  static const xxlarge = 1536;
+  static const small = 640.0;
+  static const medium = 768.0;
+  static const large = 1024.0;
+  static const xlarge = 1280.0;
+  static const xxlarge = 1536.0;
 }
