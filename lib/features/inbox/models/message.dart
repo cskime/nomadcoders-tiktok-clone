1c9f4c1d7418db09b3a3de4b377d1fb3f37e5990
Chamsol Kim <kcsol1005@gmail.com>
2024-06-19 10:58:07 +0900

    #28.2 StreamProvider, `snapshots()`, QuerySnapshot - 실시간 data 변경 알림받기

--- lib/features/inbox/models/message.dart
+++ lib/features/inbox/models/message.dart
@@ -2,17 +2,21 @@ class Message {
   Message({
     required this.text,
     required this.userId,
+    required this.createdAt,
   });
 
   final String text;
   final String userId;
+  final int createdAt;
 
   Message.fromJson(Map<String, dynamic> json)
       : text = json["text"],
-        userId = json["userId"];
+        userId = json["userId"],
+        createdAt = json["createdAt"];
 
   Map<String, dynamic> toJson() => {
         "text": text,
         "userId": userId,
+        "createdAt": createdAt,
       };
 }
--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone/features/authentication/repositories/authentication_repository.dart';
 import 'package:tiktok_clone/features/inbox/models/message.dart';
@@ -8,6 +9,23 @@ import 'package:tiktok_clone/features/inbox/repositories/messages_repository.dar
 final messagesProvider = AsyncNotifierProvider<MessagesViewModel, void>(
   () => MessagesViewModel(),
 );
+
+final chatProvider = StreamProvider<List<Message>>((ref) {
+  final database = FirebaseFirestore.instance;
+  return database
+      .collection("chat_roomos")
+      .doc("JgM17lyY08VRZ8W4xC8g")
+      .collection("texts")
+      .orderBy("createdAt")
+      .snapshots()
+      .map(
+        (snapshot) => snapshot.docs
+            .map((document) => Message.fromJson(
+                  document.data(),
+                ))
+            .toList(),
+      );
+});
 
 class MessagesViewModel extends AsyncNotifier<void> {
   late final MessagesRepository _messagesRepository;
@@ -23,9 +41,9 @@ class MessagesViewModel extends AsyncNotifier<void> {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
       final message = Message(
-        text: text,
-        userId: _authenticationRepository.user!.uid,
-      );
+          text: text,
+          userId: _authenticationRepository.user!.uid,
+          createdAt: DateTime.now().millisecondsSinceEpoch);
       _messagesRepository.sendMessage(message);
     });
   }
