739f66b8b6154108be1185112459ff63b5df23b1
Chamsol Kim <kcsol1005@gmail.com>
2024-06-23 18:31:58 +0900

    #30.4 Widget test - WidgetTester.firstWidget, find.byType, mocking MediaQuery

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -21,6 +21,78 @@ void main() {
             .color,
         Colors.white,
       );
+      expect(
+        (tester
+                .firstWidget<AnimatedContainer>(find.byType(AnimatedContainer))
+                .decoration as BoxDecoration)
+            .color,
+        const Color(0xff6750a4),
+      );
+    });
+
+    testWidgets("Disabled state test", (tester) async {
+      await tester.pumpWidget(
+        const Directionality(
+          textDirection: TextDirection.ltr,
+          child: FormButton(title: "Next", disabled: true),
+        ),
+      );
+
+      expect(find.text("Next"), findsOneWidget);
+      expect(
+        tester
+            .firstWidget<AnimatedDefaultTextStyle>(
+                find.byType(AnimatedDefaultTextStyle))
+            .style
+            .color,
+        Colors.grey.shade400,
+      );
+    });
+
+    testWidgets("Disabled state on dark mode test", (tester) async {
+      await tester.pumpWidget(
+        const MediaQuery(
+          data: MediaQueryData(
+            platformBrightness: Brightness.dark,
+          ),
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: FormButton(title: "Next", disabled: true),
+          ),
+        ),
+      );
+
+      expect(find.text("Next"), findsOneWidget);
+      expect(
+        (tester
+                .firstWidget<AnimatedContainer>(find.byType(AnimatedContainer))
+                .decoration as BoxDecoration)
+            .color,
+        Colors.grey.shade800,
+      );
+    });
+
+    testWidgets("Disabled state on light mode test", (tester) async {
+      await tester.pumpWidget(
+        const MediaQuery(
+          data: MediaQueryData(
+            platformBrightness: Brightness.light,
+          ),
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: FormButton(title: "Next", disabled: true),
+          ),
+        ),
+      );
+
+      expect(find.text("Next"), findsOneWidget);
+      expect(
+        (tester
+                .firstWidget<AnimatedContainer>(find.byType(AnimatedContainer))
+                .decoration as BoxDecoration)
+            .color,
+        Colors.grey.shade300,
+      );
     });
   });
 }
